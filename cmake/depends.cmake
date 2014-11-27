794e5f6cdac96b87ead0324bfe0ab95b80efce86
David Lin <davll.xc@gmail.com>
2014-11-28 06:49:16 +0800

    re-organize cmakefiles

---
+++ cmake/depends.cmake
@@ -0,0 +1,23 @@
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(eigen3 REQUIRED eigen3)
+include_directories(${EIGEN3_INCLUDE_DIRS})
+
+find_package(Ceres REQUIRED)
+include_directories(${CERES_INCLUDE_DIRS})
+
+find_package(Mve REQUIRED)
+include_directories(${MVE_INCLUDE_DIRS})
+
+#add_library(mve STATIC IMPORTED)
+#set_property(TARGET mve APPEND INTERFACE_INCLUDE_DIRECTORIES
+#  ${MVE_INCLUDE_DIRS}
+#  )
+#set_property(TARGET mve )
+
+#if(APPLE)
+#  add_library(ceres SHARED IMPORTED)
+#else()
+#  add_library(ceres STATIC IMPORTED)
+#endif()
+
+
