d5090aef37485fd0426cd6994316776ab389ba56
David Lin <davll.xc@gmail.com>
2014-11-28 07:53:13 +0800

    code style

--- pba/pba.cpp
+++ pba/pba.cpp
@@ -2,8 +2,13 @@
 #include <glog/logging.h>
 
 #include <mve/camera.h>
+#include <mve/depthmap.h>
 #include <mve/image.h>
 #include <mve/image_io.h>
+#include <mve/mesh.h>
+#include <mve/mesh_info.h>
+#include <mve/mesh_io.h>
+#include <mve/mesh_tools.h>
 #include <mve/view.h>
 #include <mve/scene.h>
 #include <util/arguments.h>
@@ -11,56 +16,40 @@
 #include <iostream>
 
 struct AppSettings {
+   std::string depthmap;
    std::string image;
+   std::string scene;
 };
 
 static AppSettings& _conf();
-static void _initArguments(util::Arguments& args);
+static void _setup_settings(int argc, const char * const * argv);
 
 int main(int argc, char* argv[])
 {
   google::InitGoogleLogging(argv[0]);
-
-  util::Arguments args;
-  _initArguments(args);
-  args.parse(argc, argv);
-
-  AppSettings& conf = _conf();
-  // conf.depthmap = "depth-L0";
-  conf.image = "undistored";
-
-  // Scan Arguments
-  while (util::ArgResult const* arg = args.next_result()) {
-    if (!arg->opt)
-      continue;
-
-    switch (arg->opt->sopt) {
-      // case 'd': conf.depthmap = arg->arg; break;
-      case 'i': conf.image = arg->arg; break;
-      case 'F':
-        {
-          int scale = arg->get_arg<int>();
-          // depthmap = "depth-L" + util::string::get<int>(scale);
-          conf.image = (scale == 0 ?
-                        "undistored" :
-                        "undist-L" + util::string::get<int>(scale));
-        } break;
-      default: throw std::runtime_error("Unknown option");
-    }
-  }
+  _setup_settings(argc, argv);
 
   mve::Scene::Ptr scene = mve::Scene::create();
-  scene->load_scene(args.get_nth_nonopt(0));
+  scene->load_scene(_conf().scene);
 
   const mve::Scene::ViewList& views = scene->get_views();
   for (mve::View::Ptr view : views) {
+    if (view == NULL)
+      continue;
+
+    mve::ByteImage::Ptr img = view->get_byte_image(_conf().image);
+    if (img == NULL)
+      continue;
+
+    mve::FloatImage::Ptr dmap = view->get_float_image(_conf().depthmap);
+    if (dmap == NULL)
+      continue;
+
     const mve::CameraInfo& cam = view->get_camera();
-    mve::ByteImage::Ptr img = view->get_byte_image(conf.image);
-    
-    // std::cout << img->width() << ", " << img->height() << "\n";
-    //float pos[3];
-    //cam.fill_camera_pos(pos);
-    //std::cout << pos[0] << ' ' << pos[1] << ' ' << pos[2] << "\n";
+
+    dmap.reset();
+    img.reset();
+    view->cache_cleanup();
   }
 
   return 0;
@@ -72,7 +61,42 @@ static AppSettings& _conf()
   return instance;
 }
 
-static void _initArguments(util::Arguments& args)
+static void _init_arguments(util::Arguments& args);
+
+static void _setup_settings(int argc, const char * const * argv)
+{
+  util::Arguments args;
+  _init_arguments(args);
+  args.parse(argc, argv);
+
+  AppSettings& conf = _conf();
+  conf.depthmap = "depth-L0";
+  conf.image = "undistored";
+
+  // Scan Arguments
+  while (util::ArgResult const* arg = args.next_result()) {
+    if (!arg->opt)
+      continue;
+
+    switch (arg->opt->sopt) {
+      case 'd': conf.depthmap = arg->arg; break;
+      case 'i': conf.image = arg->arg; break;
+      case 'F':
+        {
+          int scale = arg->get_arg<int>();
+          conf.depthmap = "depth-L" + util::string::get<int>(scale);
+          conf.image = (scale == 0 ?
+                        "undistored" :
+                        "undist-L" + util::string::get<int>(scale));
+        } break;
+      default: throw std::runtime_error("Unknown option");
+    }
+  }
+
+  conf.scene = args.get_nth_nonopt(0);
+}
+
+static void _init_arguments(util::Arguments& args)
 {
   args.set_exit_on_error(true);
   args.set_nonopt_minnum(1);
