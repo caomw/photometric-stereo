57c274e9f7e7f4165dcc34b210f5d2019d2670a3
David Lin <davll.xc@gmail.com>
2014-12-03 00:46:58 +0800

    working on warping

--- warp3d/main.cpp
+++ warp3d/main.cpp
@@ -1,105 +1,15 @@
 #include "app_settings.h"
 #include "warping.h"
 
-#include <mve/depthmap.h>
-#include <mve/image_io.h>
-#include <mve/mesh_io.h>
-#include <mve/view.h>
 #include <ogl/opengl.h>
-#include <ogl/mesh_renderer.h>
-#include <ogl/shader_program.h>
-#include <ogl/camera.h>
-#include <ogl/texture.h>
 #ifdef __APPLE__
 #  include <GLUT/GLUT.h>
 #endif
 
 #include <iostream>
 
-static mve::Scene::Ptr scene;
-static mve::TriangleMesh::Ptr mesh;
-static ogl::MeshRenderer::Ptr mesh_renderer;
-static ogl::ShaderProgram::Ptr warp_program; 
-
-static const char* WarpPassVertCode = 
-"#version 330 core\n"
-"in vec4 pos;\n"
-"out vec4 position;\n"
-"//uniform mat4 viewmat;\n"
-"//uniform mat4 projmat;\n"
-"void main()\n"
-"{\n"
-"  position = pos;\n"
-"  //gl_Position = projmat * (viewmat * pos);\n"
-"  gl_Position = pos;\n"
-"}\n"
-;
-
-static const char* WarpPassFragCode =
-"#version 330 core\n"
-"in vec4 position;\n"
-"uniform sampler2D view_tex;\n"
-"out vec4 color;\n"
-"void main()\n"
-"{\n"
-"  color = texture(view_tex, position.xy);\n"
-"}\n"
-;
-// TODO: position -> another camera view -> read texel
-
-static void init()
+void display()
 {
-  AppSettings& conf = AppSettings::instance();
-
-  scene = mve::Scene::create();
-  scene->load_scene(conf.scene);
-  mesh = mve::geom::load_mesh(conf.mesh);
-  if (1) {
-    mesh = mve::TriangleMesh::create();
-    auto vert = mesh->get_vertices();
-    vert.push_back(math::Vec3f(1.0, 1.0, 0.0));
-    vert.push_back(math::Vec3f(-1.0, 1.0, 0.0));
-    vert.push_back(math::Vec3f(-1.0, -1.0, 0.0));
-    vert.push_back(math::Vec3f(1.0, -1.0, 0.0));
-    auto face = mesh->get_faces();
-    face.push_back(0);
-    face.push_back(1);
-    face.push_back(2);
-    face.push_back(2);
-    face.push_back(3);
-    face.push_back(0);
-  }
-  mesh_renderer = ogl::MeshRenderer::create(mesh);
-  warp_program = ogl::ShaderProgram::create();
-  warp_program->load_vert_code(WarpPassVertCode);
-  warp_program->load_frag_code(WarpPassFragCode);
-}
-
-static void render_warp(mve::View::Ptr view,
-                        mve::View::Ptr ref_view)
-{
-  mve::ByteImage::Ptr img = view->get_byte_image(m_ImageName);
-  if (img == NULL)
-    return;
-  mve::ByteImage::Ptr ref_img = ref_view->get_byte_image(m_ImageName);
-
-  const mve::CameraInfo& cam = view->get_camera();
-  const mve::CameraInfo& ref_cam = ref_view->get_camera();
-  int width = ref_img->width(), height = ref_img->height();
-
-  auto view_tex = ogl::Texture::create();
-  view_tex->upload(img);
-  view_tex->bind();
-  mesh_renderer->set_shader(warp_program);
-  mesh_renderer->draw();
-}
-
-static void display()
-{
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-  
-
   glutSwapBuffers();
 }
 
@@ -108,14 +18,16 @@ int main(int argc, char* argv[])
   AppSettings& conf = AppSettings::initialize(argc, argv);
   glutInit(&argc, argv);
 
-  unsigned int mode = GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH;
+  unsigned int mode = GLUT_RGBA | GLUT_DOUBLE;
 #ifdef __APPLE__
   mode |= GLUT_3_2_CORE_PROFILE;
 #endif
   glutInitDisplayMode(mode);
   glutCreateWindow("warp3d");
-
   glutDisplayFunc(display);
+
+  Warping warping;
+  warping.process();
 
   return 0;
 }
--- warp3d/warping.cpp
+++ warp3d/warping.cpp
@@ -5,7 +5,11 @@
 #include <mve/image_io.h>
 #include <mve/mesh_io.h>
 #include <mve/view.h>
-// TODO: replace ogl module with glfx (by david)
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 #include <ogl/mesh_renderer.h>
 #include <ogl/shader_program.h>
 #include <ogl/camera.h>
@@ -30,7 +34,6 @@ Warping::Warping()
   m_Scene = scene;
   m_Mesh = mesh;
   m_ImageName = conf.image;
-  m_DepthMapName = conf.depthmap;
 }
 
 Warping::~Warping()
@@ -63,7 +66,10 @@ void Warping::process(int view_id)
     if (reg_img == NULL)
       continue;
 
-    // TODO: Next? Save?
+    cv::Mat cv_img(reg_img->height(), reg_img->width(), CV_8UC3,
+                   reg_img->get_byte_pointer());
+    cv::imshow("view image", cv_img);
+    cv::waitKey(0);
 
     view->cache_cleanup();
   }
@@ -71,31 +77,32 @@ void Warping::process(int view_id)
   ref_view->cache_cleanup();
 }
 
-const char* WarpPassVertCode = 
-"#version 330 core\n"
-"in vec4 pos;\n"
-"out vec4 position;\n"
-"//uniform mat4 viewmat;\n"
-"//uniform mat4 projmat;\n"
-"void main()\n"
-"{\n"
-"  position = pos;\n"
-"  //gl_Position = projmat * (viewmat * pos);\n"
-"  gl_Position = pos;\n"
-"}\n"
+const char* WarpPassVertCode =
+  "#version 330 core\n"
+  "in vec4 pos;\n"
+  "out vec4 position;\n"
+  "uniform mat4 ref_viewmat;\n"
+  "uniform mat4 ref_projmat;\n"
+  "void main()\n"
+  "{\n"
+  "  position = pos;\n"
+  "  //gl_Position = ref_projmat * (ref_viewmat * pos);\n"
+  "  gl_Position = pos;\n"
+  "}\n"
 ;
 
 const char* WarpPassFragCode =
-"#version 330 core\n"
-"in vec4 position;\n"
-"uniform sampler2D view_tex;\n"
-"out vec4 color;\n"
-"void main()\n"
-"{\n"
-"  color = texture(view_tex, position.xy);\n"
-"}\n"
+  "#version 330 core\n"
+  "in vec4 position;\n"
+  "uniform sampler2D view_tex;\n"
+  "uniform mat4 viewmat;\n"
+  "uniform mat4 projmat;\n"
+  "layout(location=0) out vec4 color;\n"
+  "void main()\n"
+  "{\n"
+  "  color = texture(view_tex, position.xy);\n"
+  "}\n"
 ;
-// TODO: position -> another camera view -> read texel
 
 mve::ByteImage::Ptr Warping::warp(mve::View::Ptr view,
                                   mve::View::Ptr ref_view)
@@ -105,9 +112,14 @@ mve::ByteImage::Ptr Warping::warp(mve::View::Ptr view,
     return mve::ByteImage::Ptr();
   mve::ByteImage::Ptr ref_img = ref_view->get_byte_image(m_ImageName);
 
-  //mve::TriangleMesh::Ptr mesh = m_Mesh;
-  auto mesh = mve::TriangleMesh::create();
-  {
+  const mve::CameraInfo& cam = view->get_camera();
+  const mve::CameraInfo& ref_cam = ref_view->get_camera();
+  int width = ref_img->width(), height = ref_img->height();
+
+  // OpenGL
+  mve::TriangleMesh::Ptr mesh = m_Mesh;
+  //auto mesh = mve::TriangleMesh::create();
+  if (0) {
     auto vert = mesh->get_vertices();
     vert.push_back(math::Vec3f(1.0, 1.0, 0.0));
     vert.push_back(math::Vec3f(-1.0, 1.0, 0.0));
@@ -121,51 +133,58 @@ mve::ByteImage::Ptr Warping::warp(mve::View::Ptr view,
     face.push_back(3);
     face.push_back(0);
   }
-
-  auto mesh_renderer = ogl::MeshRenderer::create();
-  mesh_renderer->set_mesh(mesh);
-
-  const mve::CameraInfo& cam = view->get_camera();
-  const mve::CameraInfo& ref_cam = ref_view->get_camera();
-  int width = ref_img->width(), height = ref_img->height();
-
-  auto warp_program = ogl::ShaderProgram::create();
-  warp_program->load_vert_code(WarpPassVertCode);
-  warp_program->load_frag_code(WarpPassFragCode);
+  auto mesh_renderer = ogl::MeshRenderer::create(mesh);
+  auto program = ogl::ShaderProgram::create();
+  program->load_vert_code(WarpPassVertCode);
+  program->load_frag_code(WarpPassFragCode);
+  program->bind();
+  program->send_uniform("view_tex", 0);
+  mesh_renderer->set_shader(program);
 
   auto view_tex = ogl::Texture::create();
   view_tex->upload(img);
 
-  //GLuint warp_tex, warp_fbo, depth_rbo;
-  //glGenTextures(1, &warp_tex);
-  //glGenFramebuffers(1, &warp_fbo);
-  //glGenRenderbuffers(1, &depth_rbo);
-  //glBindTexture(GL_TEXTURE_2D, warp_tex);
-  //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0,
-  //             GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, NULL);
-  //glBindFramebuffer(GL_FRAMEBUFFER, warp_fbo);
-  //glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, warp_tex, 0);
-  //glFramebufferRenderbuffer(GL_FRAMEBUFFER, )
-  //glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  GLuint color_tex, depth_rbo, fbo;
+  glGenTextures(1, &color_tex);
+  glGenRenderbuffers(1, &depth_rbo);
+  glGenFramebuffers(1, &fbo);
+  glBindTexture(GL_TEXTURE_2D, color_tex);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0,
+               GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, NULL);
+  glBindRenderbuffer(GL_RENDERBUFFER, depth_rbo);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, width, height);
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+  glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, color_tex, 0);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+                            GL_RENDERBUFFER, depth_rbo);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  glBindRenderbuffer(GL_RENDERBUFFER, 0);
+  glBindTexture(GL_TEXTURE_2D, 0);
 
-  // TODO: 3D warping
-  //glBindFramebuffer(GL_DRAW_FRAMEBUFFER, warp_fbo);
-  //glDrawBuffer(GL_COLOR_ATTACHMENT0);
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
+  glDrawBuffer(GL_COLOR_ATTACHMENT0);
+  glViewport(0, 0, width, height);
+  //glClearColor(1.0, 0.0, 0.0 , 1.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glEnable(GL_DEPTH_TEST);
+  glActiveTexture(GL_TEXTURE0);
   view_tex->bind();
-  mesh_renderer->set_shader(warp_program);
   mesh_renderer->draw();
-
-  //glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-  //glBindTexture(GL_TEXTURE_2D, 0);
   glFlush();
 
-  
-
   mve::ByteImage::Ptr registered_img = mve::ByteImage::create();
+  mve::ByteImage::Ptr out_img = registered_img;
+  out_img->allocate(width, height, 4);
 
-  //glDeleteFramebuffers(1, &warp_fbo);
-  //glDeleteTextures(1, &warp_tex);
-  //glDeleteRenderbuffers(1, &depth_rbo);
+  // Read Pixel Output
+  glBindTexture(GL_TEXTURE_2D, color_tex);
+  //view_tex->bind();
+  glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8,
+                out_img->get_byte_pointer());
 
+  // Clean up
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+
+  out_img->delete_channel(3);
   return registered_img;
 }
