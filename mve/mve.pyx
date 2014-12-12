edbd9bd81bd479c1370b5b56cce2b88277e4b409
David Lin <davll.xc@gmail.com>
2014-12-13 00:41:11 +0800

    improve mve

--- mve/mve.pyx
+++ mve/mve.pyx
@@ -6,11 +6,95 @@ import numpy
 from util.ref_ptr cimport RefPtr
 from base.image_base cimport ImageBase as CImageBase, ImageType as CImageType
 cimport base.image_base as image_base
+from base.image_base cimport ImageBase as CImageBase
 from base.camera_info cimport CameraInfo as CCameraInfo
 from base.view cimport View as CView
 from base.scene cimport Scene as CScene
 from cython.view cimport array as cvarray
 from libc.string cimport memcpy
+from cpython cimport PyObject, Py_INCREF
+
+numpy.import_array()
+
+cdef int _ImageType2NumpyDType(CImageType tp):
+    if tp == image_base.IMAGE_TYPE_UINT8:
+        return numpy.NPY_UINT8
+    elif tp == image_base.IMAGE_TYPE_UINT16:
+        return numpy.NPY_UINT16
+    elif tp == image_base.IMAGE_TYPE_UINT32:
+        return numpy.NPY_UINT32
+    elif tp == image_base.IMAGE_TYPE_UINT64:
+        return numpy.NPY_UINT64
+    elif tp == image_base.IMAGE_TYPE_SINT8:
+        return numpy.NPY_INT8
+    elif tp == image_base.IMAGE_TYPE_SINT16:
+        return numpy.NPY_INT16
+    elif tp == image_base.IMAGE_TYPE_SINT32:
+        return numpy.NPY_INT32
+    elif tp == image_base.IMAGE_TYPE_SINT64:
+        return numpy.NPY_INT64
+    elif tp == image_base.IMAGE_TYPE_FLOAT:
+        return numpy.NPY_FLOAT32
+    elif tp == image_base.IMAGE_TYPE_DOUBLE:
+        return numpy.NPY_FLOAT64
+    else:
+        raise TypeError('Unknown MVE Image Type')
+
+cdef class ImageBase:
+    cdef RefPtr[CImageBase] thisptr
+
+    @staticmethod
+    cdef create(RefPtr[CImageBase] ptr):
+        obj = ImageBase()
+        obj.thisptr = ptr
+        return obj
+
+    def __dealloc__(self):
+        self.thisptr.reset()
+        #print('image deallocated')
+
+    property width:
+        def __get__(self):
+            return self.thisptr.get().width()
+
+    property height:
+        def __get__(self):
+            return self.thisptr.get().height()
+
+    property channels:
+        def __get__(self):
+            return self.thisptr.get().channels()
+
+    property valid:
+        def __get__(self):
+            return self.thisptr.get().valid()
+
+    property byte_size:
+        def __get__(self):
+            return self.thisptr.get().get_byte_size()
+
+    cdef void* get_pointer(self):
+        return <void*> self.thisptr.get().get_byte_pointer()
+
+    cdef numpy.ndarray to_numpy(self):
+        cdef numpy.npy_intp shape[3]
+        shape[0] = <numpy.npy_intp> self.thisptr.get().height()
+        shape[1] = <numpy.npy_intp> self.thisptr.get().width()
+        shape[2] = <numpy.npy_intp> self.thisptr.get().channels()
+        cdef int ndim = 3 if shape[2] > 1 else 2
+        cdef int dtype = _ImageType2NumpyDType(self.thisptr.get().get_type())
+        cdef void *ptr = self.get_pointer()
+        return numpy.PyArray_SimpleNewFromData(ndim, shape, dtype, ptr)
+
+    def __array__(self):
+        return self.to_numpy()
+
+    property raw:
+        def __get__(self):
+            cdef numpy.ndarray arr = numpy.array(self, copy=False)
+            arr.base = <PyObject*> self
+            Py_INCREF(self)
+            return arr
 
 cdef class CameraInfo:
     cdef CCameraInfo obj
@@ -74,69 +158,61 @@ cdef class CameraInfo:
         self.obj.fill_inverse_calibration(<float*>arr.data, width, height)
         return arr
 
-
-cdef _image2ndarray(RefPtr[CImageBase] image):
-  cdef CImageBase* img = image.get()
-  cdef int height = img.height()
-  cdef int width = img.width()
-  cdef int channels = img.channels()
-  cdef CImageType imgtype = img.get_type()
-  shape = (height, width, channels)
-  if imgtype == image_base.IMAGE_TYPE_UINT8:
-      dtype = numpy.uint8
-  elif imgtype == image_base.IMAGE_TYPE_UINT16:
-      dtype = numpy.uint16
-  elif imgtype == image_base.IMAGE_TYPE_FLOAT:
-      dtype = numpy.float32
-  cdef void *ptr = <void*> img.get_byte_pointer()
-  #cdef numpy.ndarray arr = numpy.ndarray(shape=shape, dtype=dtype, order='C')
-  cdef numpy.ndarray arr = numpy.zeros(shape=shape, dtype=dtype, order='C')
-  memcpy(<void*>arr.data, ptr, img.get_byte_size())
-  return arr
-
 cdef class View:
     cdef RefPtr[CView] thisptr
-    def __init__(self):
-        pass
+
+    @staticmethod
+    cdef create(RefPtr[CView] ptr):
+        obj = View()
+        obj.thisptr = ptr
+        return obj
+
     def __dealloc__(self):
         self.thisptr.reset()
+
     property id:
         def __get__(self):
             return self.thisptr.get().get_id()
         def __set__(self, value):
             self.thisptr.get().set_id(value)
+
     property name:
         def __get__(self):
             return str(self.thisptr.get().get_name())
         def __set__(self, value):
             self.thisptr.get().set_name(value);
+
     property camera:
         def __get__(self):
             cam = CameraInfo()
             cam.obj = self.thisptr.get().get_camera()
             return cam
+
     def get_image(self, name):
         if not self.thisptr.get().has_image_embedding(name):
             return None
-        raw = self.thisptr.get().get_image(name)
-        return _image2ndarray(raw)
+        cdef RefPtr[CImageBase] img = self.thisptr.get().get_image(name)
+        return ImageBase.create(img)
+
     def cleanup_cache(self):
         self.thisptr.get().cache_cleanup()
 
 cdef class Scene:
     cdef RefPtr[CScene] thisptr
+
     def __init__(self):
         self.thisptr = CScene.create()
+
     def __dealloc__(self):
         self.thisptr.reset()
+
     def load(self, path):
         self.thisptr.get().load_scene(path)
+
     property views:
         def __get__(self):
             n_views = self.thisptr.get().get_views()
             views = []
             for ptr in n_views:
-                obj = View()
-                obj.thisptr = ptr
-                views.append(obj)
+                views.append(View.create(ptr))
             return views
--- mve/test.py
+++ mve/test.py
@@ -2,19 +2,19 @@
 import cv2
 from mve import Scene
 s = Scene()
-s.load('../tmp/b-daman')
+s.load('../tmp/b-daman/scene')
 
 v = s.views[0]
 
 cam = v.camera
-print(cam.principal_point)
-print(cam.translation_vector)
-print(cam.rotation_matrix)
-print(cam.world_to_cam_matrix)
-print(cam.cam_to_world_matrix)
+#print(cam.principal_point)
+#print(cam.translation_vector)
+#print(cam.rotation_matrix)
+#print(cam.world_to_cam_matrix)
+#print(cam.cam_to_world_matrix)
 
 img = v.get_image('undist-L1')
-print(img.shape)
+print(img.raw)
 #print(img)
-cv2.imshow("show", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+cv2.imshow("show", cv2.cvtColor(img.raw, cv2.COLOR_RGB2BGR))
 cv2.waitKey(0)
